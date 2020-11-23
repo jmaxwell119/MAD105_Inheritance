28300ac5030eb74182d242a6733b3a8bf0334692
Jason Maxwell <jmaxwell119@students.mchenry.edu>
2020-11-23 12:46:31 -0600

    changed vars to vals

--- src/main.kt
+++ src/main.kt
@@ -2,13 +2,13 @@ import java.text.DecimalFormat
 fun main(){
     val currency = DecimalFormat("$.00")
     println("Enter a number to select the type of customer.")
-    var userAns = 0
+//    var userAns = 0
     do{
         println("1) commercial customer")
         println("2) residential customer")
         println("3) done")
         print("Enter choice: ")
-        userAns = readLine()!!.toInt()
+        val userAns = readLine()!!.toInt()
         var customerType = "Error please enter a valid number"
         when (userAns){
             1 -> customerType = "commercial customer"
@@ -18,19 +18,19 @@ fun main(){
         if (userAns == 1){
             println("userAns is 1, that means commercial")
             println("enter customer name: ")
-            var customerName: String = readLine()!!
+            val customerName: String = readLine()!!
             println("enter customer phone: ")
-            var customerPhone: String = readLine()!!
+            val customerPhone: String = readLine()!!
             println("enter customer address: ")
-            var customerAddress: String = readLine()!!
+            val customerAddress: String = readLine()!!
             println("enter square footage: ")
-            var squareFootage: Double = readLine()!!.toDouble()
+            val squareFootage: Double = readLine()!!.toDouble()
             println("enter property name: ")
-            var propertyName: String = readLine()!!
+            val propertyName: String = readLine()!!
             println("are there multiple properties? enter true or false")
-            var multiProperty: Boolean = readLine()!!.toBoolean()
-            var rate: Double = 5.0
-            var customer0 = Commercial(propertyName, rate, multiProperty, customerName, customerPhone, customerAddress, squareFootage)
+            val multiProperty: Boolean = readLine()!!.toBoolean()
+            val rate = 5.0
+            val customer0 = Commercial(propertyName, rate, multiProperty, customerName, customerPhone, customerAddress, squareFootage)
             customer0.printAll()
             println("the weekly charge is " + currency.format(customer0.weeklyCharges(squareFootage, rate)))
             println("")
@@ -38,17 +38,17 @@ fun main(){
         if (userAns == 2){
             println("userAns is 2, that means residential")
             println("enter customer name: ")
-            var customerName: String = readLine()!!
+            val customerName: String = readLine()!!
             println("enter customer phone: ")
-            var customerPhone: String = readLine()!!
+            val customerPhone: String = readLine()!!
             println("enter customer address: ")
-            var customerAddress: String = readLine()!!
+            val customerAddress: String = readLine()!!
             println("enter square footage: ")
-            var squareFootage: Double = readLine()!!.toDouble()
+            val squareFootage: Double = readLine()!!.toDouble()
             println("is the customer a senior citizen? enter true or false")
-            var senior: Boolean = readLine()!!.toBoolean()
-            var rate: Double = 0.0
-            var customer0 = Residential(senior, rate, customerName, customerPhone, customerAddress, squareFootage)
+            val senior: Boolean = readLine()!!.toBoolean()
+            val rate = 0.0
+            val customer0 = Residential(senior, rate, customerName, customerPhone, customerAddress, squareFootage)
             customer0.printAll()
             println("the weekly charge is " + currency.format(customer0.weeklyCharges(squareFootage, rate)))
             println("")
